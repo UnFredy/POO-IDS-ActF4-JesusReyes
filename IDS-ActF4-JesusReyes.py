5f846207b3f47e1aa8a0aeb5635667c0f7b0c733
UnFredy <jesus_enriquexp@hotmail.com>
2025-11-10 00:30:51 -0600

    Segunda versión del código. Implementación de clase abtracta

--- IDS-ActF4-JesusReyes.py
+++ IDS-ActF4-JesusReyes.py
@@ -1,7 +1,7 @@
-
+from abc import ABC, abstractmethod
 # CLASE PRINCIPAL: Producto
 
-class Producto:
+class Producto(ABC):
     
     __limite_descuento = 0.5
     
@@ -11,12 +11,14 @@ class Producto:
         self.precio = precio
         self.stock = stock
     
+
+    @abstractmethod
     def mostrar_info(self):
-        print(f"Nombre del producto: {self.nombre}.\nDescripción: {self.descripcion}. \nPrecio: ${self.precio} \nStock: {self.stock}.")
+        pass
     
     def actualizar_stock(self, cantidad):
         self.stock += cantidad
-        print(f"Stock actualizado a: {self.stock} unidades.")
+        print(f"{self.nombre}. Stock actualizado a: {self.stock} unidades.")
         
     def mostrar_precio(self):
         print(f"${self.precio}") 
@@ -26,9 +28,9 @@ class Producto:
         if self.stock > 0:
             if self.stock - cantidad >= 0:
                 self.stock -= cantidad
-                print(f"Venta realizada! Nuevo stock: {self.stock}")
+                print(f"Venta realizada! \n{self.nombre}. Nuevo stock: {self.stock}")
             else:
-                print(f"Stock insuficiente para la venta. Stock actual: {self.stock}")
+                print(f"{self.nombre}. Stock insuficiente para la venta. Stock actual: {self.stock}. Productos requeridos: {cantidad}")
         else:
             print("No hay stock")
     
@@ -38,28 +40,46 @@ class Producto:
             raise ValueError("Descuento mayor al limite")
         self.precio = round(self.precio * (1 - descuento), 2)
     
-    def __str__(self):
-        return f"Nombre del producto: {self.nombre}.\nDescripción: {self.descripcion}. \nPrecio: ${self.precio} \nStock: {self.stock}."
 
 class Electronico(Producto):
-    pass
+    def mostrar_info(self):
+        print(f"--- ELECTRÓNICO --- \n{self.nombre}. \n{self.descripcion}. \nPrecio: ${self.precio} \nStock: {self.stock}")
+
+
 
 class Ropa(Producto):
-    pass
+    
+    def __init__(self, nombre, descripcion, precio, stock, talla):
+        super().__init__(nombre, descripcion, precio, stock)
+        self.talla = talla
+        
+    def mostrar_info(self):
+        print(f"--- ROPA --- \n{self.nombre} \n{self.descripcion} \nPrecio: ${self.precio} \nStock: {self.stock} en talla {self.talla}")
 
-class Alimento(Producto):
-    pass
 
-class Higiene(Producto):
-    pass
 
 class Videojuegos(Electronico):
-    pass
+    def __init__(self, nombre, descripcion, precio, stock, plataforma, genero):
+        super().__init__(nombre, descripcion, precio, stock)
+        self.plataforma = plataforma  
+        self.genero = genero
+        
+        
+    def mostrar_info(self):
+        print(f"--- VIDEOJUEGO --- \n{self.nombre} - {self.genero} \n{self.descripcion}. \nPrecio: ${self.precio} \nCopias disponibles: {self.stock} {self.plataforma}")
+
     
-Tele = Producto("Tele", "Televisor 42 pulgadas", 5500, 10)
+tele = Electronico("Televisor", "Televisor 42 pulgadas", 5500, 10)
+camisa = Ropa("Camisa", "Camisa de algodón color azul tipo polo", 350, 25, "M")
+juego = Videojuegos("Zelda", "Aventura en mundo abierto", 1400, 5, "Xbox", "Aventura")
+
+Productos = [tele, camisa, juego]
 
 
-print(Tele)
-Tele.aplicar_descuento(0.3)
-Tele.actualizar_stock(20)
-Tele.mostrar_info()
+for p in Productos:
+    p.mostrar_info()
+    p.aplicar_descuento(0.2)
+
+tele.actualizar_stock(20)
+camisa.venta(10)
+juego.venta(10)
